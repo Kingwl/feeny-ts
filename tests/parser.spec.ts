0feb19b53e945dab4f3cab80cc20a65a5b0556ec
kingwl <kingwenlu@gmail.com>
2021-12-05 11:53:06 +0800

    adjust test case

--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -1,189 +1,21 @@
 import type {} from 'jest'
 import { parseCode } from './utils'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const casesPath = path.resolve(__dirname, 'cases');
 
 describe('Parser', () => {
-    it('Should work with integer', () => {
-        const code = '1'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
+    const fileNames = fs.readdirSync(casesPath);
 
-    it('Should work with variable reference', () => {
-        const code = 'x'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
+    fileNames.forEach(fileName => {
+        const baseName = path.basename(fileName, '.feeny');
+        const fileNamePath = path.join(casesPath, fileName);
+        const content = fs.readFileSync(fileNamePath, 'utf8').toString();
 
-    it('Should work with printing', () => {
-        const code = 'printf (" My age is ~.\ n " , 8)'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with arrays', () => {
-        const code = 'arrays (10, 0)'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with null', () => {
-        const code = 'null'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with object', () => {
-        const code = 
-`object(p):
-    var x = 10
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with object with slots', () => {
-        const code = 
-`object(p):
-    var x = 10
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with object without extends', () => {
-        const code = 
-`object:
-    var x = 10
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-
-    it('Should work with function', () => {
-        const code =
-`
-defn f(x, y, z ):
-    null
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with method slot', () => {
-        const code = 
-`object(p):
-    var x = 10
-    method f():
-        this.x
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with arguments', () => {
-        const code = 
-`object(p):
-    var x = 10
-    method f(x, y, z):
-        this.x + x + y + z
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with method call', () => {
-        const code = 'o.f(10)'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with slot lookup', () => {
-        const code = 'o.field'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with slot assignment', () => {
-        const code = 'o.field = 42'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with function call', () => {
-        const code = 'f(10)'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with variable assignment', () => {
-        const code = 'x = 42'
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with if expression', () => {
-        const code = 
-`if x:
-    printf("A")
-else:
-    printf("B")
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with if expression with only if', () => {
-        const code = 
-`if x:
-    printf("A")
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with while expression', () => {
-        const code = 
-`while x:
-    x = x + y
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with get shorthand', () => {
-        const code = `a[1]`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with set shorthand', () => {
-        const code = `a[1] = 0`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with binary shorthand', () => {
-        const code = 
-`
-x + y
-x - y
-x * y
-x / y
-x % y
-x < y
-x > y
-x <= y
-x >= y
-x == y
-`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
-    })
-
-    it('Should work with lookup chains', () => {
-        const code = `a.b.c`
-        const file = parseCode(code);
-        expect(file).toMatchSnapshot();
+        it(baseName, () => {
+            const file = parseCode(content)
+            expect(file).toMatchSnapshot();
+        })
     })
 })
--- tests/scanner.spec.ts
+++ tests/scanner.spec.ts
@@ -1,190 +1,21 @@
 import type {} from 'jest'
 import { scanCode } from './utils'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const casesPath = path.resolve(__dirname, 'cases');
 
 describe('Scanner', () => {
-    it('Should work with integer', () => {
-        const code = '1'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
+    const fileNames = fs.readdirSync(casesPath);
 
-    it('Should work with variable reference', () => {
-        const code = 'x'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
+    fileNames.forEach(fileName => {
+        const baseName = path.basename(fileName, '.feeny');
+        const fileNamePath = path.join(casesPath, fileName);
+        const content = fs.readFileSync(fileNamePath, 'utf8').toString();
 
-    it('Should work with printing', () => {
-        const code = 'printf (" My age is ~.\ n " , 8)'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with arrays', () => {
-        const code = 'arrays (10, 0)'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with null', () => {
-        const code = 'null'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with object', () => {
-        const code = 
-`object(p):
-    var x = 10
-    method f():
-        this.x
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with object with slots', () => {
-        const code = 
-`object(p):
-    var x = 10
-    method f(x, y, z):
-        this.x + x + y + z
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with object without extends', () => {
-        const code = 
-`object:
-    var x = 10
-    method f():
-        this.x
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with method call', () => {
-        const code = 'o.f(10)'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with slot lookup', () => {
-        const code = 'o.field'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with slot assignment', () => {
-        const code = 'o.field = 42'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with function call', () => {
-        const code = 'f(10)'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with variable assignment', () => {
-        const code = 'x = 42'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with if expression', () => {
-        const code = 
-`if x:
-    printf("A")
-else:
-    printf("B")
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with if expression with only if', () => {
-        const code = 
-`if x:
-    printf("A")
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with while expression', () => {
-        const code = 
-`while x:
-    x = x + y
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with local variable', () => {
-        const code = 'var x = 42'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with local expression', () => {
-        const code = 'f(x)'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with global variable', () => {
-        const code = 'var x = 42'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with function', () => {
-        const code =
-`
-defn f(x, y, z ):
-    x + y
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with top level expression', () => {
-        const code = 'f(x)'
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with get shorthand', () => {
-        const code = `a[1]`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with set shorthand', () => {
-        const code = `a[1] = 0`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
-    })
-
-    it('Should work with binary shorthand', () => {
-        const code = 
-`
-x + y
-x - y
-x * y
-x / y
-x % y
-x < y
-x > y
-x <= y
-x >= y
-x == y
-`
-        const tokens = scanCode(code);
-        expect(tokens).toMatchSnapshot();
+        it(baseName, () => {
+            const tokens = scanCode(content)
+            expect(tokens).toMatchSnapshot();
+        })
     })
 })
