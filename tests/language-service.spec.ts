4d80a99b699a9d6d1eacc248bd757f5583f77145
kingwl <kingwenlu@gmail.com>
2021-12-10 23:36:10 +0800

    add more case

--- tests/language-service.spec.ts
+++ tests/language-service.spec.ts
@@ -30,3 +30,54 @@ bar.b
         })
     })
 })
+
+describe('Language service 1', () => {
+    const code = 
+`
+var bar = object:
+    var a = 1
+    var b = 2
+bar.a
+bar.b
+`     
+
+    const posList = [
+        code.indexOf(`bar.`),
+        code.indexOf(`bar.a`) + 'bar.'.length,
+        code.indexOf(`bar.b`) + 'bar.'.length,
+    ]
+    const ls = createLanguageService(code)
+    posList.forEach(pos => {
+        const decl = ls.goToDefinition(pos)
+        it(`Definition of ${pos}`, () => {
+            expect(decl?.__debugText).toMatchSnapshot()
+        })
+    })
+})
+
+describe('Language service 2', () => {
+    const code = 
+`
+var bar = object:
+    var a = 1
+    var b = object:
+        var c = 2
+bar.a
+bar.b
+bar.b.c
+`     
+
+    const posList = [
+        code.indexOf(`bar.`),
+        code.indexOf(`bar.a`) + 'bar.'.length,
+        code.indexOf(`bar.b`) + 'bar.'.length,
+        code.indexOf(`bar.b.c`) + 'bar.b.'.length,
+    ]
+    const ls = createLanguageService(code)
+    posList.forEach(pos => {
+        const decl = ls.goToDefinition(pos)
+        it(`Definition of ${pos}`, () => {
+            expect(decl?.__debugText).toMatchSnapshot()
+        })
+    })
+})
